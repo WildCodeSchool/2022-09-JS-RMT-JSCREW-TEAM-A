c7ed2c0e8eb119a65b90ab1e387a407d795f946e
kadev <karimaoudiaa@gmail.com>
2022-09-30 12:54:06 +0200

    add: header-menu

---
+++ script.js
@@ -0,0 +1,38 @@
+//Nav menu
+
+const btnNav = document.querySelector('.nav-mobile');
+const listeNav = document.querySelector('nav.main-nav ul');
+let imgBtn = document.querySelector('.nav-mobile img');
+
+
+
+
+btnNav.addEventListener('click', () => {
+
+    listeNav.classList.toggle('active-nav');
+
+    if(window.location.href.includes('index')){
+        if(imgBtn.src.includes('menu')){
+            imgBtn.src = 'images/croix.svg';
+            imgBtn.style.width = "30px";
+            imgBtn.style.height = "30px";
+        } else {
+            imgBtn.src = 'images/menu.svg';
+            imgBtn.style.width = "40px";
+            imgBtn.style.height = "40px";
+        }
+    
+    } else {
+        if(imgBtn.src.includes('menu')){
+            imgBtn.src = 'images/croix.svg';
+            imgBtn.style.width = "30px";
+            imgBtn.style.height = "30px";
+        } else {
+            imgBtn.src = 'images/menu.svg';
+            imgBtn.style.width = "40px";
+            imgBtn.style.height = "40px";
+        }
+    
+    }
+
+})
